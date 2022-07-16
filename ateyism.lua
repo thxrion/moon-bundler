5a8a99dd3298e6bdc3068acd41f7897066e1b3c3
THXRION666 <strike201420141@gmail.com>
2022-07-16 20:07:46 +0300

    added cyrillic characters lower/upper conversion

--- ateyism.lua
+++ ateyism.lua
@@ -17,12 +17,10 @@ local function create_dictionary_unit(pattern, substitute)
 end
 
 local function ateyism_handler(dictionary, text)
-   local result = ""
+   local words = {}
+   local 
    for word in string.gmatch(text, "%S+") do
-      for _, unit in ipairs(dictionary) do
-         local pattern = unit.pattern
-         local substitute = unit.substitute
-      end
+      table.insert(words, )
    end
    -- TODO: реалізувати заміну слова
    return u8:decode("бэбра")
@@ -50,6 +48,46 @@ end
 -- додати хук на chat bubble
 -- додати хук на введення до чату
 -- додати хук на введення до діалогу
+
+do -- cyrillic character support for string.upper and string.lower
+   local lower = string.lower
+   local sub = string.sub
+   local char = string.char
+   local upper = string.upper
+   local concat = table.concat
+
+   local lu_cyrillic, ul_cyrillic = {}, {}
+
+   for i = 192, 223 do
+      local A, a = char(i), char(i + 32)
+      ul_cyrillic[A] = a
+      lu_cyrillic[a] = A
+   end
+   
+   local E, e = char(168), char(184)
+   ul_cyrillic[E] = e
+   lu_cyrillic[e] = E
+
+   function string.nlower(s)
+      s = lower(s)
+      local len, res = #s, {}
+      for i = 1, len do
+         local ch = sub(s, i, i)
+         res[i] = ul_cyrillic[ch] or ch
+      end
+      return concat(res)
+   end
+
+   function string.nupper(s)
+      s = upper(s)
+      local len, res = #s, {}
+      for i = 1, len do
+         local ch = sub(s, i, i)
+         res[i] = lu_cyrillic[ch] or ch
+      end
+      return concat(res)
+   end
+end
 
 dictionary = {
    create_dictionary_unit("боге", "науке"),
