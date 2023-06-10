1aaa9922a3c8ac251be5c884dc79d0d98786594a
THXRION666 <strike201420141@gmail.com>
2023-06-10 19:03:38 +0300

    added require statement search method

--- src/main.c
+++ src/main.c
@@ -7,6 +7,9 @@
 
 #include "template.h"
 
+#define MAX_MODULES 256
+
+
 // args --bundle-directory "path"
 // args --working-directory "path"
 // args --entry-point "path"
@@ -18,6 +21,8 @@ char* config_bundle_directory;
 char* config_source_directory;
 char* config_entry_point;
 char* config_target;
+
+char* modules[MAX_MODULES] = {};
 
 void config_read_arguments(int argc, char* argv[]) {
     for (int i = 1; i < argc - 1; i++) {
@@ -140,7 +145,61 @@ char* get_module_definition(const char* lua_path, const char* module_code) {
     return module_definition_code;
 }
 
+const char* require_keyword = "require";
+const char* opening_quotes[] = {"'", "\"", "[["};
+const char* closing_quotes[] = {"'", "\"", "]]"}; 
+
+void find_lua_requires(const char* file_content) {
+    const char* curr_pos = file_content;
+    while ((curr_pos = strstr(curr_pos, require_keyword)) != NULL) {
+        curr_pos += strlen(require_keyword);
+
+        const char* opening_quote_pos = NULL;
+        size_t quote_index;
+        for (int i = 0; i < 3; i++) {
+            const char* quote_pos = strstr(curr_pos, opening_quotes[i]);
+
+            if (!quote_pos) {
+                continue;
+            }
+
+            if (!opening_quote_pos || quote_pos < opening_quote_pos) {
+                opening_quote_pos = quote_pos;
+                quote_index = i;
+            }
+        }
+
+        if (!opening_quote_pos) {
+            return;
+        }
+
+        curr_pos = opening_quote_pos + strlen(opening_quotes[quote_index]);
+
+        const char* closing_quote_pos = strstr(curr_pos, closing_quotes[quote_index]);
+        if (!closing_quote_pos) {
+            printf("Unclosed quote after 'require' statement.\n");
+            return;
+        }
+
+        size_t require_string_len = closing_quote_pos - curr_pos;
+        char* require_string = malloc(require_string_len + 1);
+        strncpy(require_string, curr_pos, require_string_len);
+        require_string[require_string_len] = '\0';
+
+        printf("Match: %s\n", require_string);
+
+        free(require_string);
+
+        curr_pos = closing_quote_pos + strlen(closing_quotes[quote_index]);
+    }
+}
+
 int main(int argc, char *argv[]) {
+    const char* input_string = "require(\"module1\") require \"module2\" require [[module3]]";
+    find_lua_requires(input_string);
+
+    return EXIT_SUCCESS;
+    
     if (argc < 1) {
         // look for config
     }
@@ -154,13 +213,15 @@ int main(int argc, char *argv[]) {
 
     char* main_module_path = get_source_file_absolute_path(config_source_directory, config_entry_point, ".lua");
 
-    if (!access(main_module_path, F_OK)) {
+    if (access(main_module_path, F_OK)) {
         free(main_module_path);
         config_free();
         return EXIT_FAILURE;
     }
 
     char* main_module_code = file_read(main_module_path);
+
+    printf("%s\n", main_module_code);
 
     free(main_module_path);
     free(main_module_code);
